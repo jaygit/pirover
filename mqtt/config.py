fd39b1a3f5cb8bb4fda128ad02a04c145125478e
Vincent Vedagiri <vincet@localhost.com>
2022-12-30 10:53:10 +0000

    Revert "tried to fix readme for image and urls"
    This reverts commit 39ffb6cbfed834af7a3ee382bf04022c9e838666.

--- mqtt/config.py
+++ mqtt/config.py
@@ -7,6 +7,6 @@ import os.path
 # Not using tls certificate for MQTT
 # however in the future need to look into this
 
-mqtt_server_host = ""
+mqtt_server_host = "192.168.2.99"
 mqtt_server_port = 1883 # TLS is 8883
 mqtt_keepalive = 120
